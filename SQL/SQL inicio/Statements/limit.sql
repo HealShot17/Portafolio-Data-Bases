390d54834a1ca8f1c645902dc68592e9ae97dd58
HealShot17 <albertokcrs@outlook.com>
2025-01-29 14:00:36 -0600

    Ajuste

--- SQL/SQL inicio/Statements/limit.sql
+++ SQL/SQL inicio/Statements/limit.sql
@@ -1,3 +1,3 @@
 select * from hello_mysql.users limit 3;
 
-select * from hello_mysql.users where not email = 'sara@gmail.com' or user_age = 15 LIMIT 2;
+select * from hello_mysql.users where email = 'sara@gmail.com' or user_age = 15 LIMIT 2;
